ce7095a919b759d4528bf6261b7f1809a29263fd
Achmad067 <127913237+Achmad067@users.noreply.github.com>
2023-12-26 14:55:45 +0800

    Add files via upload

---
+++ Greenfoot Project/JF_V02_S03_L03PrjStudent/RobotWorld.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class RobotWorld here.
+ * 
+ * @author (Ibad) 
+ * @version (a version number or a date)
+ */
+public class RobotWorld extends World
+{
+
+    /**
+     * Constructor for objects of class RobotWorld.
+     * 
+     */
+    public RobotWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1); 
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Robot robot = new Robot();
+        addObject(robot,292,207);
+        robot.setLocation(46,57);
+        block block = new block();
+        addObject(block,387,121);
+    }
+}
